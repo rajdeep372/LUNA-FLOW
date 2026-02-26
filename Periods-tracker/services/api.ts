1f0ef1585d242277b658e3540c2d7c9355dca55d
rajdeep <chatterjeer477@gmail.com>
2026-02-27 02:30:49 +0530

    fixed api link

--- Periods-tracker/services/api.ts
+++ Periods-tracker/services/api.ts
@@ -82,7 +82,7 @@
 // };
 import { User, PeriodLog, UserProfile } from '../types';
 
-const API_URL = 'https://lunaflow-backend.onrender.com';
+const API_URL = 'https://lunaflow-backend.onrender.com/api';
 
 export const api = {
   login: async (email: string, password: string) => {
